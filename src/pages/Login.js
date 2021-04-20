1a2cc808af1cdae0c64c5cb85daff718919ce2dd
MaS <mas@MaSs-MacBook-Pro.local>
2021-04-21 00:11:26 +0300

    Tailwind setup

---
+++ src/pages/Login.js
@@ -0,0 +1,38 @@
+import { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
+const Login = () => {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || email === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iPhone with instagram"
+          className="max-w-full"
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I will be the form</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
